806c71b586a7ae803490e31cb8480ee2c9f374c8
Pyae Phyo <pyaephyo34@gmail.com>
2024-03-07 23:40:26 +0630

    update

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -101,9 +101,147 @@ async function insertManyTasks() {
         await client.close();
     }
 }
-// insertManyTasks().catch(error => console.error(error))
+ insertManyTasks().catch(error => console.error(error))
 
 
+
+async function readUsers() {
+    const client = new MongoClient(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+
+        const db = client.db(databaseName);
+        //const result = await db.collection('users').findOne({ name:'Thandar'})
+
+        //const result = await db.collection('users').findOne({ name:'Thandar' , age:36})
+
+        const result = await db.collection('users').findOne({ _id: new ObjectId("65e9d352847df612725b9bd7")})
+
+        console.log(result)
+
+    }
+    catch(error){
+        console.error('Error',error)
+
+    }
+    finally {
+        await client.close();
+    }
+}
+
+//readUsers().catch(error => console.error(error))
+
+
+async function updateOne() {
+    const client = new MongoClient(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+
+        const db = client.db(databaseName);
+
+        const result = await db.collection('users').updateOne({
+            _id:new ObjectId('65e9d352847df612725b9bd7')
+        },{
+            $set:{
+                name: 'Mike'
+            }
+        })
+
+        console.log(result)
+
+    }
+    catch(error){
+        console.error('Error',error)
+
+    }
+    finally {
+        await client.close();
+    }
+}
+//updateOne().catch(error => console.error(error))
+
+
+async function updateMany() {
+    const client = new MongoClient(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+
+        const db = client.db(databaseName);
+
+        const result = await db.collection('tasks').updateMany({
+            completed:false
+        },{
+            $set:{
+                completed: true
+            }
+        })
+
+        console.log(result)
+
+    }
+    catch(error){
+        console.error('Error',error)
+
+    }
+    finally {
+        await client.close();
+    }
+}
+//updateMany().catch(error => console.error(error))
+
+
+async function DeleteMany() {
+    const client = new MongoClient(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+
+        const db = client.db(databaseName);
+
+        const result = await db.collection('tasks').deleteMany({
+            completed:true
+        })
+
+        console.log(result)
+
+    }
+    catch(error){
+        console.error('Error',error)
+
+    }
+    finally {
+        await client.close();
+    }
+}
+//DeleteMany().catch(error => console.error(error))
+
+async function DeleteOne() {
+    const client = new MongoClient(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+
+        const db = client.db(databaseName);
+
+        const result = await db.collection('tasks').deleteOne({
+            completed:false
+        })
+
+        console.log(result)
+
+    }
+    catch(error){
+        console.error('Error',error)
+
+    }
+    finally {
+        await client.close();
+    }
+}
+DeleteOne().catch(error => console.error(error))
 
 // MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
 //     if (error) {
@@ -111,30 +249,38 @@ async function insertManyTasks() {
 //     }
 
 //     const db = client.db(databaseName)
-    
-//     db.collection('users').insertOne({
-//         name: 'Andrew',
-//         age: 27
+
+//     db.collection('users').updateOne({
+//         _id:'65e9d352847df612725b9bd7'
+//     },{
+//         $set:{
+//             name: 'Mike'
+//         }
+//     }).then((result)=>{
+//         console.log(result)
+//     }).catch((error)=>{
+//         console.log(error)
 //     })
+
 // })
 
 
-MongoClient.connect(connectionURL,{ useNewUrlParser:true},(error,client)=>{
+// MongoClient.connect(connectionURL,{ useNewUrlParser:true},(error,client)=>{
 
-    if(error){
-        return console.log(error)
-    }
+//     if(error){
+//         return console.log(error)
+//     }
 
-    const db = client.db(databaseName)
+//     const db = client.db(databaseName)
 
-    db.collection('users').findOne({ name:'Thandar'},(error,result)=>{
+//     db.collection('users').findOne({ name:'Thandar'},(error,result)=>{
 
-        if(error){
-            console.log(error)
-        }
+//         if(error){
+//             console.log(error)
+//         }
 
-        console.log(result)
+//         console.log(result)
 
-    })
+//     })
 
-})
+// })
---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'Andrew',
+    age: 34
+})
+
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+})
+
+const Task = mongoose.model('task',{
+    description:{
+        type: String
+    },
+    completed:{
+        type: Boolean
+    }
+})
+
+const task = new Task({
+    description : 'Learn the Mongoose Library',
+    completed: false
+})
+
+task.save().then(()=>{
+    console.log(task)
+}).catch((error)=>{
+    console.log('Error!',error)
+})
